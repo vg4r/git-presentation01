e1498f2b3f5bdf5cb8feabb80c770d6dae87e0f7
Vugar <s.vugar1988@gmail.com>
2024-02-26 10:18:36 +0100

    Fix code review comment : Return list of resources instead of one object instance.

--- src/main/java/com/example/demo/api/DemoController.java
+++ src/main/java/com/example/demo/api/DemoController.java
@@ -5,14 +5,19 @@ import com.example.demo.dto.DemoDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class DemoController {
 
 
     @GetMapping("/demo")
-    public DemoDto getAll(){
+    public List<DemoDto> getAll(){
+        List<DemoDto>  list = new ArrayList<>();
+        list.add(new DemoDto());
         // ....
-        return new DemoDto();
+        return list;
     }
 
 }
