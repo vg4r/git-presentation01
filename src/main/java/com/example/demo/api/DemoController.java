fb4674398740ac3eddb071df39c9902253a593e9
Vugar <s.vugar1988@gmail.com>
2024-02-26 10:14:42 +0100

    Add getAll method to get all required data

--- src/main/java/com/example/demo/api/DemoController.java
+++ src/main/java/com/example/demo/api/DemoController.java
@@ -1,11 +1,18 @@
 package com.example.demo.api;
 
 
+import com.example.demo.dto.DemoDto;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
 
+    @GetMapping("/demo")
+    public DemoDto getAll(){
+        // ....
+        return new DemoDto();
+    }
 
 }
